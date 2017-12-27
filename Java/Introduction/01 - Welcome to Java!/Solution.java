801d8373481b6fb6d6dbb90a13f3bc85a7bbfcbc
CarlosGmail <carlos.er7@gmail.com>
2017-12-27 13:27:02 -0200

    Adicionando 3 soluções

---
+++ Java/Introduction/01 - Welcome to Java!/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * 		@author Carlos Reis
+ * 		Github: github.com/CarlosEReis
+ * 	HackerRank: hackerrank.com/carlos_er7
+ */
+ 
+public class Solution {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+		
+	}
+	
+}
+
---
+++ SQL/Basic Select/01 - Revising the Select Query I.sql
@@ -0,0 +1,9 @@
+--     Author: Carlos Reis
+--     Github: github.com/CarlosEReis
+-- HackerRank: hackerrank.com/carlos_er7
+
+SELECT * 
+FROM CITY 
+WHERE 
+    COUNTRYCODE = 'USA' 
+    AND POPULATION > 100000;
